05313865c72437b245b69d7e90bbd81fede7a10b
bogdanMerkulow <bogdanMerkulow@mail.ru>
2021-08-11 14:32:04 +0300

    Add grid to recycler view and fix layout

--- app/src/main/java/com/example/cards/fragments/CardsFragment.kt
+++ app/src/main/java/com/example/cards/fragments/CardsFragment.kt
@@ -2,17 +2,18 @@ package com.example.cards.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cards.R
 import com.example.cards.adapters.RecyclerViewAdapter
 import com.example.cards.databinding.CardsFragmentBinding
 import com.example.cards.factories.CardViewHolderFactory
 import com.example.cards.models.Card
+
 
 class CardsFragment : Fragment() {
     private lateinit var adapter: RecyclerViewAdapter<Card>
@@ -27,10 +28,12 @@ class CardsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val cardList: RecyclerView = binding.rwCardList
+        cardList.layoutManager = GridLayoutManager(context, 4)
 
         adapter = RecyclerViewAdapter(
             CardViewHolderFactory(),
-            R.layout.card_item
+            R.layout.card_item,
+            {Log.e("test123", "click item")}
         )
 
         cardList.adapter = adapter
@@ -40,6 +43,11 @@ class CardsFragment : Fragment() {
                 Card("test", 1, ""),
                 Card("test", 2, ""),
                 Card("test", 3, ""),
+                Card("test", 4, ""),
+                Card("test", 5, ""),
+                Card("test", 6, ""),
+                Card("test", 7, ""),
+                Card("test", 8, ""),
             )
         )
     }
