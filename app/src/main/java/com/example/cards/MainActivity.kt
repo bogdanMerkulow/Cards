9b7d84af0acd4227b2f3cf192ca7029b3e3a9716
bogdanMerkulow <bogdanMerkulow@mail.ru>
2021-08-11 12:18:20 +0300

    Create main fragment

--- app/src/main/java/com/example/cards/MainActivity.kt
+++ app/src/main/java/com/example/cards/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.cards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cards.fragments.CardsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val transaction = supportFragmentManager.beginTransaction()
+        val fragment = CardsFragment()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
